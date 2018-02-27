ba145a3d40dbf6172a23c469c0fd743367b5984d
Travis Clarkson <tk1667@gmail.com>
2018-02-27 00:15:01 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,4 +3,8 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion,
     :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
+  def initialize(attributes)
+    attributes.send {|key, value| (":key="), value }
+  end
+
 end
